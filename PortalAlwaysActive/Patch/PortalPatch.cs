e28623c5c70ec0d12b472e310d4a437048ac4a06
LloydHawkeye <nicolas.lange33@gmail.com>
2024-04-10 00:28:35 +0200

    Commit V1.0.0

---
+++ PortalAlwaysActive/Patch/PortalPatch.cs
@@ -0,0 +1,23 @@
+﻿using Fusion;
+using UnityEngine;
+
+namespace PortalAlwaysActive.Patch
+{
+    internal class PortalPatch
+    {
+        public static void patch()
+        {
+            On.Portal.StartActivationTimer += Portal_StartActivationTimer;
+        }
+
+        private static void Portal_StartActivationTimer(On.Portal.orig_StartActivationTimer orig, Portal self)
+        {
+            if (self.HasStateAuthority)
+            {
+                self.Active = false;
+                //Désactivation du timer
+                self.ActivationTimer = TickTimer.CreateFromSeconds(self.Runner, 0.5f);
+            }
+        }
+    }
+}
